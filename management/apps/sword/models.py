8c191aa8dcdc7063cd7aacb5f53656011c63d7d5
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2021-04-07 11:57:19 +0800

    Revert "Revert "feat: 增加社交信息表 账户验证表""
    This reverts commit 342fb54b511bea43232055db61c6fe1259f5ac39.

--- management/apps/sword/models.py
+++ management/apps/sword/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import UserManager
 from django.db import models
 
-
 # Create your models here.
 from management.apps.core.models import TimestampedModel
 
@@ -36,15 +35,57 @@ class User(AbstractUser, TimestampedModel):
         help_text="用户头像",
         null=True
     )
-    phone = models.CharField(
-        help_text="手机号",
-        null=True,
-        max_length=11
-    )
     USERNAME_FIELD = 'id'
 
     def __str__(self):
         return str(self.id)
+
+
+class UserSocialInfo(TimestampedModel):
+    user = models.ForeignKey(
+        User,
+        db_constraint=False,
+        on_delete=models.CASCADE,
+        related_name='social_info'
+    )
+    uid = models.CharField(
+        help_text='账户验证信息',
+        max_length=40
+    )
+    provider = models.CharField(
+        help_text='账户提供方',
+        max_length=15
+    )
+
+    class Meta:
+        db_table = 'sword_user_social_info'
+
+
+class AccountConfirmation(TimestampedModel):
+    user = models.ForeignKey(
+        User,
+        db_constraint=False,
+        on_delete=models.CASCADE,
+        related_name='account_confirmation'
+    )
+    verification_account = models.CharField(
+        max_length=20,
+        help_text='验证账户'
+    )
+    verification_credential = models.CharField(
+        max_length=20,
+        help_text='验证凭证'
+    )
+    credential_expiration_date = models.DateTimeField(
+        help_text='凭证过期时间'
+    )
+    credential_group = models.CharField(
+        max_length=20,
+        help_text='凭证分组'
+    )
+
+    class Meta:
+        db_table = 'sword_account_confirmation'
 
 # todo 设计用户 =>
 #
